c58a878ac21cb773b7014d0f71588fb6559f7317
Christopher Ash <ashcl@iupui.edu>
2017-07-21 19:34:29 -0400

    The Lottonaut slightly improves. Added a uniform distribution function with user defined parameters.

--- LottonautLauncher.py
+++ LottonautLauncher.py
@@ -1,5 +1,6 @@
-import src.Lottonaut as lotnot
+import src.Lottonaut as ln
 
-if(__name__ =="__main__"):
-    lottonaut = lotnot.Lottonaut()
+if (__name__ =="__main__"):
+    lottonaut = ln.Lottonaut()
     print(lottonaut.newNormalDist())
+    print(lottonaut.newUniformDistribution(50, 1, 65))
--- src/Lottonaut.py
+++ src/Lottonaut.py
@@ -4,10 +4,16 @@ import numpy as np
 class Lottonaut:
     def __init__(self):
         pass
-    def newNormalDist(selfself):
-        norm = tf.random_normal([1000], mean=25, stddev=10)
+
+    def newNormalDist(self):
+        normTensor = tf.random_normal([1000], mean=25, stddev=10)
         with tf.Session() as session:
-            return norm.eval()
+            return normTensor.eval()
+
+    def newUniformDistribution(self, count=10, min=1, max=65):
+        uniformTensor = tf.random_uniform([count], minval=min, maxval=max, dtype=tf.float32)
+        with tf.Session() as session:
+            return uniformTensor.eval()
 
 
 if (__name__ == "__main__"):
