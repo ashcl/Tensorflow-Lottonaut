d5a9fcafef6c8f39db0a8dd23243a210a31e5791
Christopher Ash <ashcl@iupui.edu>
2017-07-21 20:01:51 -0400

    The Lottonaut gains readablility and helper function. Added parsing for readability of numbers.

--- LottonautLauncher.py
+++ LottonautLauncher.py
@@ -2,5 +2,4 @@ import src.Lottonaut as ln
 
 if (__name__ =="__main__"):
     lottonaut = ln.Lottonaut()
-    print(lottonaut.newNormalDist())
-    print(lottonaut.newUniformDistribution(50, 1, 65))
+    print(lottonaut.guessWhiteBalls(1, 65))
--- src/Lottonaut.py
+++ src/Lottonaut.py
@@ -1,21 +1,24 @@
 import tensorflow as tf
-import numpy as np
 
 class Lottonaut:
     def __init__(self):
-        pass
+        self.state = "Powerball"
 
     def newNormalDist(self):
         normTensor = tf.random_normal([1000], mean=25, stddev=10)
         with tf.Session() as session:
             return normTensor.eval()
 
-    def newUniformDistribution(self, count=10, min=1, max=65):
+    def newUniformDistribution(self, count, min=1, max=65):
         uniformTensor = tf.random_uniform([count], minval=min, maxval=max, dtype=tf.float32)
         with tf.Session() as session:
             return uniformTensor.eval()
 
+    def guessWhiteBalls(self, min, max):
+        balls = self.newUniformDistribution(5, min, max)
+        for idx, ball in enumerate(balls):
+            balls[idx] = int(round(ball))
+        return balls
 
 if (__name__ == "__main__"):
-    lottonaut = Lottonaut()
-    print(lottonaut.newNormalDist())
+    print("Performing Lottonaut systems check. Stand by...")
